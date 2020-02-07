76bfa0a418d1af8c862a25a93875aecc3a5d7575
killarock <yury.beloborodov@gmail.com>
2020-02-07 15:25:54 +0300

    quiz-creatore whithout server

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import classes from './Input.scss';
+import React from "react";
+import classes from "./Input.scss";
 
 function isInvalid({ valid, touched, shouldValidate }) {
   return !valid && shouldValidate && touched;
 }
 
 const Input = props => {
-  const inputType = props.type || 'text';
+  const inputType = props.type || "text";
   const cls = [classes.Input];
   const htmlFor = `${inputType}-${Math.random()}`;
 
@@ -15,10 +15,17 @@ const Input = props => {
   }
 
   return (
-    <div className={cls.join(' ')}>
+    <div className={cls.join(" ")}>
       <label htmlFor={htmlFor}>{props.label}</label>
-      <input id={htmlFor} type={inputType} value={props.value} onChange={props.onChange} />
-      {isInvalid(props) && <span>{props.errorMsg || 'Введите верное значение'}</span>}
+      <input
+        id={htmlFor}
+        type={inputType}
+        value={props.value}
+        onChange={props.onChange}
+      />
+      {isInvalid(props) && (
+        <span>{props.errorMsg || "Введите верное значение"}</span>
+      )}
     </div>
   );
 };
